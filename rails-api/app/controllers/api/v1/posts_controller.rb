40aba207527b0b031be51d112eeabe44ff8a0aef
Taylor Jones <taylorjonessoftware@gmail.com>
2016-10-21 11:45:14 -0400

    rails mock complete

--- rails-api/app/controllers/api/v1/posts_controller.rb
+++ rails-api/app/controllers/api/v1/posts_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::PostsController < ApplicationController
   def index
     render json: Post.all
   end
+
+  def show
+    render json: Post.find(params[:id])
+  end
 end
--- rails-api/spec/factories/post_factory.rb
+++ rails-api/spec/factories/post_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :post do
-    title { Faker::Name.name }
-    body { Faker::Internet.email }
+    title { Faker::Lorem.words(4) }
+    body { Faker::Lorem.paragraph(10) }
     published true
   end
 end
